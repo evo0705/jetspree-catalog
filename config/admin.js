1a16e80c1d3a75ff6da7fa3ab256d3e36015f314
Ravin <ravindran@jetspree.com>
2018-05-02 15:27:19 +0800

    added websocket env variable

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,11 @@
 // config used by dashboard client side only
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001/api/v1';
+const API_WEB_SOCKET_URL = process.env.API_WEB_SOCKET_URL || 'ws://localhost:3001';
+
 module.exports = {
   // dashboard UI language
   language: 'en',
-  apiBaseUrl: 'http://localhost:3001/api/v1',
-  apiWebSocketUrl: 'ws://localhost:3001',
+  apiBaseUrl: API_BASE_URL,
+  apiWebSocketUrl: API_WEB_SOCKET_URL,
   developerMode: true
 }
--- config/store.js
+++ config/store.js
@@ -1,6 +1,8 @@
 // config used by store client side only
+const AJAX_BASE_URL = process.env.AJAX_BASE_URL || 'http://localhost:3001/ajax';
+
 module.exports = {
   // store UI language
   language: 'en',
-  ajaxBaseUrl: 'http://localhost:3001/ajax'
+  ajaxBaseUrl: AJAX_BASE_URL
 }
