0b7f5237c23ed742ce906964b1fa3c3072f9d99c
Ravin <ravindran@jetspree.com>
2018-05-03 17:48:59 +0800

    implemented cloudinary support for product images

---
+++ src/api/server/services/products/cloudinary.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const cloudinary = require('cloudinary');
+const mongo = require('../../lib/mongo');
+const ObjectID = require('mongodb').ObjectID;
+const MAX_SIZE = 2048
+
+class CloudinaryService {
+  constructor() {}
+
+  async getImages(productObjectID) {
+    return mongo.db.collection('products').findOne({ _id: productObjectID }, {fields: {images: 1}}).then(product => {
+      if(product && product.images && product.images.length > 0) {
+        let images = product.images.map(image => {
+          image.url = cloudinary.url(image.external_id, { quality: "auto", fetch_format: "auto", secure: true })
+          return image;
+        })
+
+        images = images.sort((a,b) => (a.position - b.position ));
+        return images;
+      } else {
+        return []
+      }
+    })
+  }
+
+  async addImage(productId, req) {
+    let uploadedFiles = [];
+    const imageFiles = req.files;
+    const dataURIs = imageFiles.map(image => {
+      return "data:" + image.mimetype + ";base64," + image.buffer.toString("base64");
+    })
+    const uploadedImages = await this.uploadImageURIs(dataURIs);
+
+    return uploadedImages.map(image => {
+      return {
+        "id": new ObjectID(),
+        "alt": "",
+        "position": 99,
+        "external_id": image.public_id
+      }
+    });
+  }
+
+  async uploadImageURIs(URIs) {
+    try {
+      return await Promise.all(URIs.map(async URI => {
+        try {
+          return await cloudinary.v2.uploader.upload(URI, {
+            width:  MAX_SIZE,
+            height: MAX_SIZE,
+            crop:   "limit",
+            format: "png",
+          });
+        } catch (err) {
+          return err
+        }
+      }))
+    } catch (err) {
+      console.error("Cloudinary upload failed:", err)
+      return err;
+    }
+  }
+}
+
+module.exports = new CloudinaryService();
