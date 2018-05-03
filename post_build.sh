c2c481cdc6ff55c1c287a40e49c0bc9abe054693
Ravin <ravindran@jetspree.com>
2018-05-03 11:33:17 +0800

    fixed the bash script for heroku postbuild

--- post_build.sh
+++ post_build.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # run npm run build only when the PROCFILE env variable is store.Procfile
-if [ ${PROCFILE} == "store.Procfile" ]
-then
+if [[ $PROCFILE == 'store.Procfile' ]]; then
    rm -rf ./dist;
    npm run build;
 fi
