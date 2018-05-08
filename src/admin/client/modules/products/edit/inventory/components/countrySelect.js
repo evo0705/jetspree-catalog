6a196735b468e6d2ea6ef9a7b0003bbcc71798bc
Ravin <ravindran@jetspree.com>
2018-05-08 11:37:10 +0800

    added comments to the countrySelect component

--- src/admin/client/modules/products/edit/inventory/components/countrySelect.js
+++ src/admin/client/modules/products/edit/inventory/components/countrySelect.js
@@ -10,11 +10,21 @@ class CountrySelect extends Component {
   render() {
     const { props } = this;
     const { labelText } = props;
-    
+
     return (
       <div className={style.countryHints}>
         <label>{ labelText }</label>
         <Select options={countries} {...props} {...props.input} multi simpleValue onBlur={() => {}} />
+        {
+          // Note: here I spread props and props.input as the Field component
+          // passes down this input object containing the value, onChange, onBlur, onFocus etc.
+          // So spreading the props passes the props to the component, and then spreading props.input
+          // passes the onChange, value etc props that Redux Form provides down to the React Select component
+          // as it expects them.
+
+          // onBlur is a hack to overwrite the Select component's default behaviour,
+          // which by default will clear the values if its not assigned to state.
+        }
       </div>
     );
   }
