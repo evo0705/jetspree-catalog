d9328351f982f8c6e30ade8f878d4c163ba14187
Samuel Lee <evo0705@gmail.com>
2018-06-07 12:05:18 +0800

    fix the display for variant values

--- src/admin/client/modules/products/edit/variantValues/index.js
+++ src/admin/client/modules/products/edit/variantValues/index.js
@@ -4,7 +4,6 @@ import VariantList from "./components/VariantList"
 
 const mapStateToProps = (state, ownProps) => {
   const { productId } = ownProps.match.params
-console.log(state.products)
   return {
     variantValues:  state.products.editProduct ? state.products.editProduct.variant_values : null,
     productId: productId,
